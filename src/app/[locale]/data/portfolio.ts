82335b42f8a6f38e1c82bc796787511201507cf0
Felipe Gomes <code@lfng.dev>
2026-09-14 18:39:00 -0300

    Update portfolio.ts

--- src/app/[locale]/data/portfolio.ts
+++ src/app/[locale]/data/portfolio.ts
@@ -28,7 +28,7 @@ export const projects: Project[] = [
 });
 
 export const jobs: Job[] = [
-  { key: "automind", company: "Automind", count: 6 },
+  { key: "automind", company: "Automind", count: 7 },
   { key: "branddi", company: "Branddi", count: 5 },
   { key: "eisa", company: "EISA", count: 4 },
   { key: "parallel", company: "Parallel Consulting & Training", count: 3 },
